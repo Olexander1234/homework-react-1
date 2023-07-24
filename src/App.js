e54d17e7940c60148b968fb7b1158bb5ed024de9
Olexander <cmurtimur@gmail.com>
2023-07-24 17:45:30 +0300

    іпі

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
-
 import './App.css';
-import { Greeting } from './components/Greeting';
-import { Message } from './components/Message';
-function App() {
-  return  (
-    <>
-      <Greeting name="Іван"/>
-       <Message text="Hello world!"/>
-    </>
-  
-  )
-  
-}
+import { Profile } from './components/homework-2/Profile';
+import user from './user.json';
+import { TransactionHistory } from './components/homework-2/TransactionHistory';
+import transaction from './transactions.json';
 
+const { username, tag, location, avatar, stats } = user;
+
+function App() {
+  return (
+    <>
+      <Profile
+        username={username}
+        tag={tag}
+        location={location}
+        avatar={avatar}
+        stats={stats}
+      />
+
+      {transaction.map(({ id, type, amount, currency }) => (
+        <TransactionHistory
+          key={id}
+          type={type}
+          amount={amount}
+          currency={currency}
+        />
+      ))}
+    </>
+  );
+}
 
 export default App;
